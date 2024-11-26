875a610508f632ba263ab47efddbec3c29b585ca
Can Vardar <ismailcanvardar@gmail.com>
2024-11-26 16:23:39 +0300

    chore: add global types

---
+++ src/types.d.ts
@@ -0,0 +1,27 @@
+export { };
+
+declare global {
+  enum Action {
+    ADD,
+    REMOVE,
+    UPDATE,
+  }
+
+  interface IQueue {
+    enqueue(item: Item): void;
+    dequeue(): string | undefined;
+    peek(): Item | undefined;
+  }
+
+  type Item = {
+    proxy: string,
+    content: Object,
+    action: Action,
+  };
+
+  type QueueNode = {
+    id: string,
+    item: Item,
+    next?: QueueNode,
+  };
+};
